dc068a7f942b69aede39289ee9a12d719a5cad86
LUCY05261015 <a2gis1015@gmail.com>
2024-07-12 09:02:17 +0900

    Add files via upload

---
+++ BLE_Bypass.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial bleSerial(A0, A1); // RX, TX
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("BLE Shield Test Started!");
+  bleSerial.begin(9600);
+}
+
+void loop() {
+  if(bleSerial.available())
+    Serial.write(bleSerial.read());
+  if(Serial.available())
+    bleSerial.write(Serial.read());
+}
